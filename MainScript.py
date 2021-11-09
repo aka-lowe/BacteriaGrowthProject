9ae4950ca5f8aea8346e9a7d7263fa4c4518803f
isabella-df <93315810+isabella-df@users.noreply.github.com>
2021-11-09 11:30:30 +0100

    Menu

--- MainScript.py
+++ MainScript.py
@@ -12,3 +12,39 @@ from PlottingFunction import dataPlot
 
 
 #hi
+def print_menu(): 
+    print(20*"-","Welcome to the action menu!",20*"-")
+    print("1. Load Data")
+    print("2. Filter Data")
+    print("3. Display Statistics")
+    print("4. Generate Plots")
+    print("5. Quit")
+    print (67*"-")
+
+loop=True
+
+while loop:
+    print_menu()
+    selection = input("Please select from above [1-5]")
+    if selection==1:
+        print("Load Data has been selected")
+        filename=input('Please enter the name of the file you wish to use: ')
+        data=dataLoad(filename)
+    elif selection==2:
+        print("Filter Data has been selected")
+        filename=input('Please enter the name of the file you wish to use: ')
+        data=dataLoad(filename)
+    elif selection==3:
+        print("Display Statistics has been selected")
+        filename=input('Please enter the name of the file you wish to use: ')
+        data=dataLoad(filename)  
+    elif selection==4:
+        print("General Plots has been selected")
+        filename=input('Please enter the name of the file you wish to use: ')
+        data=dataLoad(filename)  
+    elif selection==5:
+        print("Quit has been selected")
+        loop=False 
+    else:
+        print("Please choose a number from 1-5. Press any key to try again.")
+    
